11826b368f3cdd174112d9fed4800cc3f86bd865
Intezar Ahmad <31982418+theintezar@users.noreply.github.com>
2022-01-17 13:39:44 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Todo from './Todo'
+
+function App() {
+  return (
+    <div>
+     <Todo h="Mobile Operation System" l="Android" l2="Blackberry" l3 = "iPhone" l4 = "Window Phone"></Todo>
+     <Todo h="Mobile Manufacturers" l="Sumsung" l2="HTC" l3="Micromax" l4="Apple"/>
+    </div>
+  )
+}
+
+export default App
+
---
+++ src/Todo.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Todo({h, l, l2, l3, l4}) {
+    return (
+        <div>
+            <h1>{h}</h1>
+            <li>{l}</li>
+            <li>{l2}</li>
+            <li>{l3}</li>
+            <li>{l4}</li>
+        </div>
+    )
+}
+
+export default Todo
